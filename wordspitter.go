712af40526b325eba0fc24f436b8e6aeef6a5f57
Ir0n2 <98989500+Ir0n2@users.noreply.github.com>
2022-08-03 13:56:13 -0300

    Add files via upload

---
+++ wordspitter.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main () {
+
+var F int
+
+var C string
+
+fmt.Println("TYPE A NUMBER")
+
+fmt.Scanf("%d", &F)
+
+fmt.Println("TYPE A WORD")
+
+fmt.Scanf("%s", &C)
+
+for n := 1 ; n < F; n++ {
+
+fmt.Println(C)
+
+	}
+}
